b364b79e52d29f753002dd5d4cf329265518089c
Nitindubji <93260752+Nitindubji@users.noreply.github.com>
2024-05-06 23:26:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ os.makedirs("./downloads", exist_ok=True)
 
 bot = Client(
     "CW",
-    api_id= 20491966,
-    api_hash= "aa1c8f86db7f78fe9bfdd77bb48a5b23",
-    bot_token= "7183698213:AAFkrfBIiWN5lGcFH_OdK0bsggv2gcFOtCY")
+    api_id= 739395,
+    api_hash= "adhjaavb",
+    bot_token= "2677777&dddddddd")
 
 logger = logging.getLogger()
 # thumb = os.environ.get("THUMB")
